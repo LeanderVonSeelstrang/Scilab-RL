c584c06f071652a384e8f1af1004ad951e7e15b1
annika <annika.oesterdiekhoff@uni-bielefeld.de>
2024-02-16 14:09:10 +0100

    register gridworld env with input noise

--- src/custom_envs/register_envs.py
+++ src/custom_envs/register_envs.py
@@ -168,6 +168,10 @@ def register_custom_envs():
     register(id="GridworldEnv-v0",
              entry_point="custom_envs.grid_world.grid_world_env:GridWorldEnv",
              max_episode_steps=10)
+    register(id="GridworldEnv-v1",
+             entry_point="custom_envs.grid_world.grid_world_env:GridWorldEnv",
+             kwargs={'is_it_possible_that_input_noise_is_applied': True},
+             max_episode_steps=10)
 
     for n_objects in range(3):
         register(id=f'Hook-o{n_objects}-v1',
