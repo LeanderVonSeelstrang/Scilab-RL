59ea879b03b7743a8a715874c67527649ebfea7c
annika <annika.oesterdiekhoff@uni-bielefeld.de>
2024-03-19 13:21:23 +0100

    remove deprecated code

--- src/custom_envs/register_envs.py
+++ src/custom_envs/register_envs.py
@@ -176,9 +176,6 @@ def register_custom_envs():
              entry_point="custom_envs.grid_world.grid_world_env:GridWorldEnv",
              kwargs={'scene_of_input_noise': True},
              max_episode_steps=10)
-    register(id="GridworldMetaEnv-v0",
-             entry_point="custom_envs.grid_world.meta_env_gridworld:MetaEnvGridworld",
-             max_episode_steps=10)
 
     for n_objects in range(3):
         register(id=f'Hook-o{n_objects}-v1',
