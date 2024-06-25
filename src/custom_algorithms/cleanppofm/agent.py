fix prediction error calculation location and change corrective for negative reward

src/custom_algorithms/cleanppofm/agent.py
@@ -63,13 +63,12 @@ class Agent(nn.Module):
             obs = torch.tensor(obs, device=device, dtype=torch.float32).detach().clone()
         return self.critic(obs)
 
-    def get_action_and_value_and_forward_model_prediction_and_prediction_error(self, fm_network, obs, env_name: str,
-                                                                               action=None,
-                                                                               deterministic: bool = False,
-                                                                               logger: Logger = None,
-                                                                               position_predicting: bool = False) -> \
+    def get_action_and_value_and_forward_model_prediction(self, fm_network, obs, action=None,
+                                                          deterministic: bool = False,
+                                                          logger: Logger = None,
+                                                          position_predicting: bool = False) -> \
             tuple[
-                torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.distributions.Normal, float]:
+                torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.distributions.Normal]:
         """
         Get the action, logarithmic probability of action distribution, entropy of the action distribution
         value of the critic network and forward model prediction in form of a normal distribution
@@ -134,44 +133,6 @@ class Agent(nn.Module):
         else:
             forward_model_prediction_normal_distribution = fm_network(obs, forward_normal_action.float())
 
-        ##### CALCULATE PREDICTION ERROR #####
-        # prediction error version one -> standard deviation
-        # prediction_error = forward_normal.stddev.mean().item()
-        # prediction error version two -> Euclidean distance
-        # calculate manually prediction error (Euclidean distance)
-        if env_name == "GridWorldEnv":
-            # predicted location can only be between 0 and 4
-            max_distance_in_gridworld = math.sqrt(((4 - 0) ** 2) + ((4 - 0) ** 2) + ((4 - 0) ** 2) + ((4 - 0) ** 2))
-            predicted_location = torch.tensor(
-                [min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][0])), 4),
-                 min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][1])), 4),
-                 min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][2])), 4),
-                 min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][3])), 4)])
-            prediction_error = (math.sqrt(torch.sum((predicted_location - obs) ** 2))) / max_distance_in_gridworld
-        elif env_name == "MoonlanderWorldEnv":
-            # we just care for the x position of the moonlander agent, because the y position is always equally to the size of the agent
-            if position_predicting:
-                positions = get_position_of_observation(obs)
-
-                # Smallest x position of the agent is the size of the agent
-                # Biggest x position of the agent is the width of the moonlander world - the size of the agent
-                first_possible_x_position = self.env.get_attr("first_possible_x_position")[0]
-                last_possible_x_position = self.env.get_attr("last_possible_x_position")[0]
-                max_distance_in_moonlander_world = math.sqrt(
-                    (last_possible_x_position - first_possible_x_position) ** 2)
-                predicted_x_position = torch.tensor([min(max(first_possible_x_position,
-                                                             forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[
-                                                                 0][0]),
-                                                         last_possible_x_position)])
-                prediction_error = (math.sqrt(
-                    torch.sum((predicted_x_position - positions[0]) ** 2))) / max_distance_in_moonlander_world
-            # TODO: implement prediction error calculation for moonlander world env with predicting whole observation
-            else:
-                raise NotImplementedError(
-                    "Prediction error calculation not implemented for MoonlanderWorldEnv with predicting whole observation!")
-        else:
-            raise ValueError("Environment not supported")
-
         # TODO: put prediction of fm network into observation? --> standard deviation or whole observation?
         ##### LOGGING #####
         # std describes the (un-)certainty of the prediction of each pixel
@@ -197,5 +158,9 @@ class Agent(nn.Module):
         #  but if you feed your cat vs dog classifier a picture containing both a cat and a dog,
         #  you probably want the model to give you a 50-50 split which is maximal entropy.
         # value of critic network, forward model prediction in normal distribution
+        print("positions", positions)
+        print("action", action)
+        print("forward_model_prediction_normal_distribution", forward_model_prediction_normal_distribution.mean)
+
         return action.unsqueeze(0), distribution.log_prob(action), distribution.entropy(), self.critic(
-            obs), forward_model_prediction_normal_distribution, prediction_error
+            obs), forward_model_prediction_normal_distribution

src/custom_algorithms/cleanppofm/cleanppofm.py
@@ -16,7 +16,8 @@ from custom_algorithms.cleanppofm.forward_model import ProbabilisticSimpleForwar
     ProbabilisticForwardNetPositionPrediction, ProbabilisticSimpleForwardNetIncludingReward, \
     ProbabilisticForwardNetPositionPredictionIncludingReward
 from custom_algorithms.cleanppofm.utils import flatten_obs, get_reward_estimation_of_forward_model, \
-    get_position_of_observation, get_next_observation_gridworld, get_reward_with_future_reward_estimation_corrective
+    get_position_of_observation, get_next_observation_gridworld, get_reward_with_future_reward_estimation_corrective, \
+    calculate_prediction_error
 from custom_algorithms.cleanppofm.agent import Agent
 from utils.custom_buffer import CustomDictRolloutBuffer as DictRolloutBuffer
 from utils.custom_buffer import CustomRolloutBuffer as RolloutBuffer
@@ -233,10 +234,9 @@ class CLEANPPOFM:
                 self.train_fm(observations, next_observations, actions, rewards_of_env)
                 #####
 
-                _, log_prob, entropy, values, _, _ = self.policy.get_action_and_value_and_forward_model_prediction_and_prediction_error(
+                _, log_prob, entropy, values, _ = self.policy.get_action_and_value_and_forward_model_prediction(
                     fm_network=self.fm_network,
                     obs=observations,
-                    env_name=self.env_name,
                     action=actions, logger=self.logger,
                     position_predicting=self.position_predicting)
                 values = values.flatten()
@@ -372,9 +372,9 @@ class CLEANPPOFM:
         while elements_in_rollout_buffer < self.n_steps:
             with torch.no_grad():
                 # get action and forward model prediction
-                actions, log_probs, _, values, forward_normal, prediction_error = self.policy.get_action_and_value_and_forward_model_prediction_and_prediction_error(
+                actions, log_probs, _, values, forward_normal = self.policy.get_action_and_value_and_forward_model_prediction(
                     fm_network=self.fm_network,
-                    obs=self._last_obs, env_name=self.env_name, logger=self.logger,
+                    obs=self._last_obs, logger=self.logger,
                     position_predicting=self.position_predicting)
 
             # log logarithmic probability of action distribution (one value in tensor)
@@ -403,6 +403,13 @@ class CLEANPPOFM:
             # dones = terminated or truncated
             new_obs, rewards, dones, infos = env.step(clipped_actions)
             rewards_of_env = copy.deepcopy(rewards)
+
+            ##### CALCULATING PREDICTION ERROR #####
+            prediction_error = calculate_prediction_error(env_name=self.env_name, env=env,
+                                                          next_obs=torch.tensor(new_obs, device=device),
+                                                          forward_model_prediction_normal_distribution=forward_normal,
+                                                          position_predicting=self.position_predicting)
+            print("prediction_error", prediction_error)
 
             if self.reward_predicting:
                 ##### FLATTING OBSERVATIONS FOR FUTURE REWARD ESTIMATION #####
@@ -612,10 +619,9 @@ class CLEANPPOFM:
             the model's action and the next hidden state (used in recurrent policies)
         """
         with torch.no_grad():
-            action, _, _, _, forward_model_prediction_normal_distribution, _ = self.policy.get_action_and_value_and_forward_model_prediction_and_prediction_error(
+            action, _, _, _, forward_model_prediction_normal_distribution = self.policy.get_action_and_value_and_forward_model_prediction(
                 fm_network=self.fm_network,
                 obs=observation,
-                env_name=self.env_name,
                 deterministic=deterministic,
                 logger=self.logger,
                 position_predicting=self.position_predicting)

src/custom_algorithms/cleanppofm/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -101,8 +102,8 @@ def get_reward_with_future_reward_estimation_corrective(rewards: torch.Tensor, f
     # different reward estimation for positive and negative rewards
     if rewards_with_future_reward_estimation < 0:
         # negative rewards
-        reward_with_future_reward_estimation_corrective = rewards_with_future_reward_estimation - (
-                abs(rewards_with_future_reward_estimation) * prediction_error)
+        reward_with_future_reward_estimation_corrective = rewards_with_future_reward_estimation + (
+                abs(rewards_with_future_reward_estimation) * (1 - prediction_error))
     else:
         # positive rewards
         if not prediction_error == 0:
@@ -165,3 +166,59 @@ def get_next_observation_gridworld(observations: torch.Tensor, actions: torch.Te
             np.concatenate((standard_agent_location, observations[index][2:4].cpu())), device=device
         )
     return agent_location_without_input_noise
+
+
+def calculate_prediction_error(env_name: str, env, next_obs, forward_model_prediction_normal_distribution: torch.normal,
+                               position_predicting: bool) -> float:
+    """
+    Calculate the prediction error between the next obs and the forward model prediction.
+    Args:
+        env_name: name of the environment
+        env: environment
+        next_obs: observation after actually executing the action
+        forward_model_prediction_normal_distribution: prediction of next observation by forward model
+        position_predicting: if the forward model is predicting the position or actual observation
+
+    Returns:
+        prediction error between the next obs and the forward model prediction
+    """
+    ##### CALCULATE PREDICTION ERROR #####
+    # prediction error version one -> standard deviation
+    # prediction_error = forward_normal.stddev.mean().item()
+    # prediction error version two -> Euclidean distance
+    # calculate manually prediction error (Euclidean distance)
+    if env_name == "GridWorldEnv":
+        # predicted location can only be between 0 and 4
+        max_distance_in_gridworld = math.sqrt(((4 - 0) ** 2) + ((4 - 0) ** 2) + ((4 - 0) ** 2) + ((4 - 0) ** 2))
+        predicted_location = torch.tensor(
+            [min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][0])), 4),
+             min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][1])), 4),
+             min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][2])), 4),
+             min(max(0, round(forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[0][3])), 4)])
+        prediction_error = (math.sqrt(torch.sum((predicted_location - next_obs) ** 2))) / max_distance_in_gridworld
+    elif env_name == "MoonlanderWorldEnv":
+        # we just care for the x position of the moonlander agent, because the y position is always equally to the size of the agent
+        if position_predicting:
+            positions = get_position_of_observation(next_obs)
+            print("actual positions: ", positions)
+
+            # Smallest x position of the agent is the size of the agent
+            # Biggest x position of the agent is the width of the moonlander world - the size of the agent
+            first_possible_x_position = env.get_attr("first_possible_x_position")[0]
+            last_possible_x_position = env.get_attr("last_possible_x_position")[0]
+            max_distance_in_moonlander_world = math.sqrt(
+                (last_possible_x_position - first_possible_x_position) ** 2)
+            predicted_x_position = torch.tensor([min(max(first_possible_x_position,
+                                                         forward_model_prediction_normal_distribution.mean.cpu().detach().numpy()[
+                                                             0][0]),
+                                                     last_possible_x_position)])
+            prediction_error = (math.sqrt(
+                torch.sum((predicted_x_position - positions[0]) ** 2))) / max_distance_in_moonlander_world
+        # TODO: implement prediction error calculation for moonlander world env with predicting whole observation
+        else:
+            raise NotImplementedError(
+                "Prediction error calculation not implemented for MoonlanderWorldEnv with predicting whole observation!")
+    else:
+        raise ValueError("Environment not supported")
+
+    return prediction_error