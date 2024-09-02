83921e5e03697595e864e64378ca7e0b60ddfb17
Manfred Eppe <manfred.eppe@tuhh.de>
2024-09-02 11:15:50 +0200

    Removed also hook and button unlock envs.

--- src/custom_envs/register_envs.py
+++ src/custom_envs/register_envs.py
@@ -133,21 +133,6 @@ def register_custom_envs():
     #     max_episode_steps=700,
     # )
 
-    register(id='Reach1DOF-v0',
-             entry_point='custom_envs.reach1dof.reach1dof_env:Reach1DOFEnv',
-             max_episode_steps=50)
-
-    for n_objects in range(3):
-        register(id=f'Hook-o{n_objects}-v1',
-                 entry_point='custom_envs.hook.hook_env:HookEnv',
-                 kwargs={'n_objects': n_objects},
-                 max_episode_steps=max(50, 100 * n_objects))
-
-        register(id=f'ButtonUnlock-o{n_objects}-v1',
-                 entry_point='custom_envs.button_unlock.button_unlock_env:ButtonUnlockEnv',
-                 kwargs={'n_buttons': n_objects+1},
-                 max_episode_steps=max(50, 50*n_objects))
-
     register(
         id='parking-limited-v0',
         entry_point='highway_env.envs:ParkingEnv',
